dde9bcb507dbf67f84af9a2b0f54febfd17cc491
bpoole53 <bpoole53@gmail.com>
2023-09-06 12:30:26 -0500

    added code to signup

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,9 +4,9 @@ const userRoutes = require('./user-routes');
 const cartRoutes = require('./cart-routes');
 const productRoutes = require('./product-routes');
 
-/*router.use('/auth', authRoutes);
+router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/cart', cartRoutes);
-router.use('/product', productRoutes);*/
+router.use('/product', productRoutes);
 
 module.exports = router;
