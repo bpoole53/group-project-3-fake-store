fea53b8cce587cd9b8aa79ccd628e47272a923cc
Annelise <sccsannelise@gmail.com>
2023-09-06 12:00:43 -0500

    fixing alignment on forms

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,10 +3,10 @@ export default function LoginForm () {
 
   return (
     <div className="justify-center">
-      <input type="text" placeholder="Username" className="input input-bordered w-full max-w-xs" />
-      <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs" />
-      <button className="btn">Login</button>
-      <a className="link" href="/signup">Don't Have an Account? Click Here to Sign Up Instead!</a>
+      <input type="text" placeholder="Username" className="input input-bordered w-full max-w-xs justify-center" />
+      <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs justify-center" />
+      <button className="btn justify-center">Login</button>
+      <a className="link justify-center" href="/signup">Don't Have an Account? Click Here to Sign Up Instead!</a>
     </div>
   )
 }
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -5,12 +5,12 @@ export default function SignupForm () {
 
   return (
     <div className="justify-center">
-      <input type="text" placeholder="First Name" className="input input-bordered w-full max-w-xs" />
-      <input type="text" placeholder="Last Name" className="input input-bordered w-full max-w-xs" />
-      <input type="text" placeholder="Username" className="input input-bordered w-full max-w-xs" />
-      <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs" />
-      <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs" />
-      <button className="btn">Sign Up</button>
+      <input type="text" placeholder="First Name" className="input input-bordered w-full max-w-xs justify-center" />
+      <input type="text" placeholder="Last Name" className="input input-bordered w-full max-w-xs justify-center" />
+      <input type="text" placeholder="Username" className="input input-bordered w-full max-w-xs justify-center" />
+      <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs justify-center" />
+      <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs justify-center" />
+      <button className="btn justify-center">Sign Up</button>
     </div>
   )
 }
