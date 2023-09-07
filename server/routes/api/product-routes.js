ab99a9cf451ea809727a06dcd9f23c0b9cc7f523
Peter Lauer <lauerpeter11@gmail.com>
2023-09-07 10:51:11 -0500

    added an i.d to products and finished the routes and controller

---
+++ server/routes/api/product-routes.js
@@ -0,0 +1,19 @@
+
+
+const express = require("express");
+const router = express.Router();
+const productController = require("../controllers/productController");
+
+// POST create a new product
+router.post("/", productController.createProduct);
+
+// PUT update a product by ID
+router.put("/:id", productController.updateProductById);
+
+// GET ALL products
+router.get("/", productController.getAllProducts);
+
+// DELETE a product by ID
+router.delete("/:id", productController.deleteProduct);
+
+module.exports = router;
