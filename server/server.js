1f4d791834999ec5b7fcd956da48059ee1a943ed
Peter Lauer <lauerpeter11@gmail.com>
2023-09-08 13:51:40 -0500

    products seeded into the database

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ dbConnection.once('open', () => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api", apiRoutes);
+//app.use("/api", apiRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client/build')));
