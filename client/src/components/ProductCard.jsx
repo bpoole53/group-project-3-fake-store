10b122ff67536f924928cadf524b07e8faa66f98
Peter Lauer <lauerpeter11@gmail.com>
2023-09-11 12:49:58 -0500

    testing server side call for products

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,24 +1,34 @@
-import '../App.css'
 
-export default function ProductCard () {
-
-  const getProducts = [];
-  function sendtoProductPage () {
-    window.location.replace('/productpage')
-  }
-  
-  return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-      <figure><img src="https://dummyimage.com/250/ffffff/000000" alt="Shoes" /></figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p className="card-price">$300</p>
-        <button className="btn btn-primary productBtn" onClick={sendtoProductPage}>Details</button>
-      </div>
-    </div>
-
-    /* {getProducts.map( p => (
-		<ProductCard product = {p}/>))} */
-
-  )
-}
+      
+      import React, { useEffect, useState } from 'react';
+      import '../App.css';
+      
+      export default function ProductCard() {
+        const [products, setProducts] = useState([]);
+      
+        useEffect(() => {
+          fetch('/api/products')
+            .then((response) => response.json())
+            .then((data) => {
+              setProducts(data); 
+            })
+            .catch((error) => {
+              console.error('Error fetching product data:', error);
+            });
+        }, []);
+      
+        return (
+          <div>
+            {products.map((product) => (
+              <div className="card card-compact w-96 bg-base-100 shadow-xl" key={product.id}>
+                <figure><img src={product.image} alt={product.name} /></figure>
+                <div className="card-body">
+                  <h2 className="card-title">{product.name}</h2>
+                  <p className="card-price">${product.price.toLocaleString()}</p>
+                  <button className="btn btn-primary productBtn">Details</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        );
+      }
