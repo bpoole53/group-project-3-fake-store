8111aea2fa9a0d4d06c656c8a94add7badf45833
Jon A <jon.jumangi@gmail.com>
2023-09-11 12:01:02 -0500

    hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import '../App.css'
+import React, {useRef, useEffect} from 'react';
+
 
 export default function Hero () {
 
@@ -6,9 +8,59 @@ export default function Hero () {
     window.location.replace('/products')
   }
 
+  const videoSources = [
+    '/video1.mp4',
+    '/video2.mp4',
+    '/video3.mp4',
+    '/video4.mp4'
+  ];
+
+  videoSources.forEach((videoSrc) => {
+    const video = new Audio(videoSrc);
+    video.preload = 'auto';
+    video.load();
+  });
+  const videoRef = useRef(null);
+ 
+
+  useEffect(() => {
+    let currentVideoIndex = 0;
+
+    const playNextVideo = () => {
+      currentVideoIndex = (currentVideoIndex +1) % videoSources.length;
+      videoRef.current.src = videoSources[currentVideoIndex];
+      videoRef.current.play();
+    };
+
+    videoRef.current.addEventListener('ended', playNextVideo);
+
+    videoRef.current.src = videoSources[currentVideoIndex];
+    videoRef.current.play();
+
+    return ()  => {
+      videoRef.current.removeEventListener('ended', playNextVideo);
+    };
+  },[videoSources]);
+
+
+
+
+  
   return (
     <>
-      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80)'}}>
+       <div className="hero">
+      <video ref={videoRef} autoPlay muted loop>
+        <source src={videoSources[0]} type="video/mp4" />
+      </video>
+      </div>
+      
+
+  
+
+
+
+
+      {/* <div className="hero min-h-screen" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80)'}}>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
@@ -17,7 +69,7 @@ export default function Hero () {
           <button className="btn btn-primary" onClick={sendtoProducts}>Shop All</button>
         </div>
        </div>
-      </div>
+      </div> */}
     </>
   )
 }
