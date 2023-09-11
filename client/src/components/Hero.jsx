4a2519a4b260de01e734509b984033fce41ecce1
Jon A <jon.jumangi@gmail.com>
2023-09-11 12:55:40 -0500

    herochanges

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -56,21 +56,18 @@ export default function Hero () {
   
   return (
     <>
-    <div className= "main">
-       <div className="hero">
+      <div className="hero">
       <video ref={videoRef} autoPlay muted loop>
         <source src={videoSources[0]} type="video/mp4" />
       </video>
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay bg-opacity-60"/>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">The waters are calling...</h1>
           <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          <button className="btn btn-primary" onClick={sendtoProducts}>Shop All</button>
-        </div>
-       </div>
-      
-    </div>
+          <button className="btn btn-primary" onClick={sendtoProducts}>Shop All Watercraft</button>
+      </div>
+      </div>
     </div>
     </>
   )
