64ff33ca7796959db17e4db03569a719716a523b
mocha <mocha@localhost.localdomain>
2023-09-12 12:01:29 -0500

    product page is populated. However props to product card seem not to work.

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,18 +1,19 @@
 import '../App.css'
 
-export default function ProductCard () {
+export default function ProductCard (props) {
 
-  const getProducts = [];
+  // const getProducts = [];
+  console.log(props)
   function sendtoProductPage () {
     window.location.replace('/productpage')
   }
   
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
-      <figure><img src="https://dummyimage.com/250/ffffff/000000" alt="Shoes" /></figure>
+      <figure><img src="https://dummyimage.com/250/ffffff/000000" alt="shoes" /></figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p className="card-price">$300</p>
+        <h2 className="card-title">{props.product.name}</h2>
+        <p className="card-price">${props.product.price}</p>
         <button className="btn btn-primary productBtn" onClick={sendtoProductPage}>Details</button>
       </div>
     </div>
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCardContainer from "../components/ProductCardContainer";
-import ProductCard from "../components/ProductCard";
 import '../App.css'
-
+import ProductCard from "../components/ProductCard";
 export default function Products() {
 
-	// call whatever is used to get the product data from mongo. Should return array anyways.
-	const getProducts = [];
+	const ogProductData = [];
+	const [productData, setProductData] = useState(ogProductData);
+	useEffect(() => {
+        getProducts()
+    }, [])
+	// call get route of controller
+	const getProducts = async () => {
+        const query = await fetch('/api/product', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const result = await query.json()
+		console.log(result)
+		setProductData(result.payload)
+		return result;
+        // implement logic for success or fail
 
+	};
+
+	// getProducts();
 	return (
 		<>
 		<div className="productPageContainer">
 		<h1 className="productTitle">Products</h1>
 			<ProductCardContainer/>
-			{getProducts.map( p => (
-			<ProductCard product = {p}/>))}
+			{productData.map( p => (<ProductCard product={p}/>))}
 		</div>
 		</>
 	)
