5e4a1ad73d26f42327ef267b4e68fe7536483f83
Peter Lauer <lauerpeter11@gmail.com>
2023-09-12 15:04:52 -0500

    switch up on cart schema to be included within the user model for simplified interface

--- server/models/User.js
+++ server/models/User.js
@@ -1,45 +1,62 @@
 
 
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose'); 
 
-const bcrypt = require("bcrypt")
+const bcrypt = require('bcrypt');
 
+const cartItemSchema = new Schema({
+  product_name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+  productDetails: {
+    price: {
+      type: Number, 
+      required: true,
+    },
+  },
+});
+
+// Define the user schema
 const userSchema = new Schema({
   fname: { 
     type: String, 
     required: true,
   },
-
   lname: {
     type: String,
     required: true,
   },
-
   email: {
     type: String,
     required: true,
     unique: true,
     match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
   },
-
   password: {
     type: String,
     required: true,
     minlength: 6,
   },
+  cart: [cartItemSchema], 
 });
 
-userSchema.method("verify", async function(pw){
-  return await bcrypt.compare(pw, this.password)
-})
+userSchema.method('verify', async function (pw) {
+  return await bcrypt.compare(pw, this.password);
+});
 
-userSchema.pre("save", async function(next){
+userSchema.pre('save', async function (next) {
   if (!this._id) {
-    this._id = new Schema.Types.ObjectId(); 
+    this._id = new mongoose.Types.ObjectId();
   }
-  this.password = await bcrypt.hash(this.password, 10)
-  next()
-})
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 const User = model('User', userSchema);
 
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,25 +1,21 @@
+
+
 const db = require('../config/connection');
-
 const dbClean = require('./dbClean');
-const {Cart,Product,User} = require('../models')
+const { Product, User } = require('../models');
 
-const productData = require('./productData.json')
-const userData = require('./userData.json')
-const cartData = require('./cartData.json'); 
+const productData = require('./productData.json');
+const userData = require('./userData.json');
 
 db.once('open', async () => {
     await dbClean('Product', 'products');
     await dbClean('User', 'users');
-  
-    await Product.insertMany(productData);
-  
-    console.log('Products seeded!');
-    
-    await User.insertMany(userData);
-    console.log('Users seeded!');
 
-    await Cart.insertMany(cartData);
-    console.log('Carts seeded!')
-    
+    await Product.insertMany(productData);
+    console.log('Products seeded!');
+
+    await User.insertMany(userData);
+    console.log('Users and Carts seeded!');
+
     process.exit(0);
-  });
+});
