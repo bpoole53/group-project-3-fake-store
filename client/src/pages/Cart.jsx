1add450dbdac168fdbafc32752f7ab3547e04776
Annelise <sccsannelise@gmail.com>
2023-09-13 15:29:22 -0500

    added cart page logic draft

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ProductCard from "../components/ProductCard";
 import CartTotal from "../components/CartTotal";
 import { useEffect, useState } from 'react';
 import { useAppContext } from '../utils/AppContext';
@@ -9,7 +8,7 @@ export default function Cart() {
 	const [ carts, setCarts ] = useState([])
 
 	useEffect(() => {
-		fetch(`/api/cart/${userData.id}`, {
+		fetch(`/api/cart/${userData._id}`, {
 			method: "GET",
 			headers: {
         "Content-Type": "application/json"
@@ -27,7 +26,15 @@ export default function Cart() {
 
 			<div className="flex flex-col w-full lg:flex-row">
 				<div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-					
+					{carts.map((cart) => (
+				<div className="card w-96 bg-base-100 shadow-xl">
+					<div className="card-body">
+						<h2 className="card-title">{cart.product_name}</h2>
+						<h3>{cart.quantity}</h3>
+						<h3>${cart.productDetails}</h3>
+					</div>
+				</div>
+					))}
 				</div> 
 				<div className="divider lg:divider-horizontal">OR</div> 
 				<div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center"><CartTotal/></div>
